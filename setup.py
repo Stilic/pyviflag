a5c70f5dbcbef9863a3b05e8c7bba3f03f5d5828
Stylix58 <lateman-jpeg@outlook.fr>
2021-01-24 09:28:54 +0100

    update first for pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyViFlag",
+    version="0.0.1",
+    author="Stylix58Alt",
+    author_email="lateman-jpeg@outlook.fr",
+    description="A unofficial Python API wrapper for Flag.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Stylix58/pyviflag",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
