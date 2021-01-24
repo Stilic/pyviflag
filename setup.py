47e7504eae06f977a763b05b2494698b5f5b11ce
Stylix58 <lateman-jpeg@outlook.fr>
2021-01-24 10:00:01 +0100

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="PyViFlag",
-    version="1.0.0",
+    version="1.0.1",
     author="Stylix58Alt",
     author_email="lateman-jpeg@outlook.fr",
     description="A unofficial Python API wrapper for Flag.",
